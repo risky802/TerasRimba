b02a81ba338045c6693c42b504682d11cc4d9822
riskyCipCip <riskyardiansyah802@gmail.com>
2026-01-12 23:27:54 +0700

    Initial commit Teras Rimba (Flutter project)

--- terasrimba/lib/Components/Login/LoginComponent.dart
+++ terasrimba/lib/Components/Login/LoginComponent.dart
@@ -1,2 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:terasrimba/Components/Login/LoignForm.dart';
+import 'package:terasrimba/size_config.dart';
+import 'package:terasrimba/utils/constants.dart';
 
-decorations
+class LoginComponent extends StatefulWidget {
+@override
+  _LoginComponent createState()=> _LoginComponent();
+  }
+}
+class _LoginComponent extends State<LoginComponent>{
+  @override
+  
+  Widget build(BuildContext context){
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsetsGeometry.symmetric(
+            horizontal: getProportionateScreenHeight(20)),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: SizeConfig.screenHeight * 0.04,),
+                  SizedBox(height: SizeConfig.screenHeight * 0.04,),
+                  SimpleShadow(
+                    child: Image.asset(
+                      "assets/images/icon.png",
+                      height: 150,
+                      width: 150,,
+                      )
+                      Opacity opacity: 0.5,
+                      color: kSecondaryColor,
+                      offset: Offset(5, 5),
+                      sigma: 2,           
+                  )
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Login !",
+                          style: mTitleStyle,
+                        )
+                      ],
+                    ),
+                  
+                  )
+                  SizedBox(
+                    height: 20,
+
+                  ),
+                  SingInform()
+                ],
+
+              ),
+            ),
+          ),
+      ),
+    );
+  }
+  }
+}
--- terasrimba/lib/Components/Register/Registrasi.dart
+++ terasrimba/lib/Components/Register/Registrasi.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:terasrimba/Components/Login/LoginComponent.dart';
 import 'package:terasrimba/size_config.dart';
 
-class LoginSecreens extends StatelessWidget {
+class RegisterScreen extends StatelessWidget {
 
-  static String routName = "/sing_in";
+  static String routName = "/sing_up";
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
       ),
-      body: LoginComponent(),
     )
   }
 }
