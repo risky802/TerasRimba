7f23c4c06c3dfa0dbe3b96f3d03f11b59d02013b
riskyCipCip <riskyardiansyah802@gmail.com>
2026-01-12 23:02:22 +0700

    Initial commit Teras Rimba (Flutter project)

---
+++ terasrimba/lib/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:terasrimba/Screens/Login/LoginSecreens.dart';
+
+fial Map<String, WidgetBuilder> routes = {
+  LoginScreen.routName(context) => LoginSecreen()
+};
